45be7d5a75b4eadbbac39e306d7306b82c64100f
Vamsi Krishna Gandikota <34382945+kittuvamsi2767@users.noreply.github.com>
2021-11-27 08:35:41 -0800

    Update SampleController.java

--- src/main/java/com/bt/contoller/SampleController.java
+++ src/main/java/com/bt/contoller/SampleController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-/*veera shekar    heee */
 @RestController
 @RequestMapping("/")
 public class SampleController {
