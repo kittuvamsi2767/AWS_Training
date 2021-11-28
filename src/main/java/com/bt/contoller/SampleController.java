7c9334630e861136966ac26554bd1f4a51a49406
Vamsi Krishna Gandikota <34382945+kittuvamsi2767@users.noreply.github.com>
2021-11-28 04:39:01 -0800

    Update SampleController.java

--- src/main/java/com/bt/contoller/SampleController.java
+++ src/main/java/com/bt/contoller/SampleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-/*vinay*/
+/*vinay.*/
 @RequestMapping("/")
 public class SampleController {
     @RequestMapping(value = "/getJson", method = RequestMethod.GET)
