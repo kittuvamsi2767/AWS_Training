d9f2b58705f7e2cbe75ad5fd08da30b13851c017
Vamsi Krishna Gandikota <34382945+kittuvamsi2767@users.noreply.github.com>
2021-12-01 07:21:06 -0800

    Update deploy.sh

--- deploy_scripts/deploy.sh
+++ deploy_scripts/deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+aws ec2 start-instances --instance-ids i-094ece8eabd8ff10c
 ansible-playbook /var/lib/jenkins/workspace/DEV_maven_webpipeline/deploy_scripts/playbook/dev_webserver.yml
 scp /var/lib/jenkins/workspace/DEV_maven_webpipeline/target/dev-maven-web-project-1.1-SNAPSHOT.war root@192.168.1.180:/var/www/html
 
